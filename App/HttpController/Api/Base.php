f7fc7327a4560d172dca9026669922e2ee4be2e7
lvzhenchao <1370164516@qq.com>
2021-12-01 22:16:06 +0800

    Create Base.php

---
+++ App/HttpController/Api/Base.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\HttpController\Api;
+
+use EasySwoole\Core\Http\AbstractInterface\Controller;
+
+/**
+ * 基础类库
+ * Class Base
+ * @package App\HttpController\Api
+ */
+class Base extends Controller
+{
+    function index()
+    {
+//        $data = [
+//            'id' =>1,
+//            'name'=>"imooc"
+//        ];
+//        return $this->writeJson(200,'ok', $data);
+//        $this->response()->write('I am category');
+    }
+
+
+}
