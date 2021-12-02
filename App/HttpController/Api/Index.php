96eabf0d755782cca9110f73bb469efff62e8f33
lvzhenchao <1370164516@qq.com>
2021-12-02 10:45:54 +0800

    Update Index.php

--- App/HttpController/Api/Index.php
+++ App/HttpController/Api/Index.php
@@ -20,7 +20,21 @@ class Index extends Base
 
     public function getVideo() {
         $db = Di::getInstance()->get('MYSQL');
-        $result = $db->where("id", 15)->getOne("video");
+
+        //写入
+//        $data = Array (
+//            "name" => "test product",
+//            "url" => "baidu.com",
+//            "content" => "百度内容",
+//        );
+//        $id = $db->insert ("video", $data);
+
+        //单条
+//        $result = $db->where("id", 15)->getOne("video");
+
+        //所有数据
+//        $result = $db->get("video");
+        $result = $db->get("video", 2);//limit
         return $this->writeJson(200,'ok', $result);
     }
 
