c6bfd25145b059868bfc06feb73a5ed514f6cc0d
lvzhenchao <1370164516@qq.com>
2021-12-12 14:36:45 +0800

    封装接口

--- App/HttpController/Api/Upload.php
+++ App/HttpController/Api/Upload.php
@@ -11,9 +11,20 @@ class Upload extends Base
     public function file() {
         $request = $this->request();
 
+        //将当前接口既支持图片和视频
+        //第一种
+        $files = $request->getSwooleRequest()->files;
+        $types = array_keys($files);
+        $type = $types[0];
+        if ($type == "image") {
+            $obj = new Image($request);
+        } else if($type == "video") {
+            $obj = new Video($request);
+        }
+
         try {
 //            $obj = new Video($request);
-            $obj = new Image($request);
+//            $obj = new Image($request);
             $file = $obj->upload();
         } catch (\Exception $e) {
             return $this->writeJson(400, $e->getMessage(), []);
