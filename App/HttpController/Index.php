2f51378b9e5236db637826a06d4a8666deea6091
lvzhenchao <1370164516@qq.com>
2021-12-28 23:02:00 +0800

    Update Index.php

--- App/HttpController/Index.php
+++ App/HttpController/Index.php
@@ -2,12 +2,17 @@
 
 namespace App\HttpController;
 
+use App\Lib\AliyunSdk\AliVod;
 use EasySwoole\Core\Http\AbstractInterface\Controller;
 class Index extends Controller
 {
     function index()
     {
         $this->response()->write('I am index');
+    }
+
+    public function testali() {
+
     }
 
 }
