118763d6fd26b03f90731363d37dc6aca605bacb
lvzhenchao <1370164516@qq.com>
2021-12-29 22:59:04 +0800

    Update AliyunVodUploader.php

--- App/lib/AliyunSdk2/uploader/AliyunVodUploader.php
+++ App/lib/AliyunSdk2/uploader/AliyunVodUploader.php
@@ -11,7 +11,7 @@ use OSS\Core\OssUtil;
 class AliyunVodUploader
 {
 
-    public function __construct($accessKeyId, $accessKeySecret, $apiRegionId=null)
+    public function __construct($accessKeyId, $accessKeySecret, $apiRegionId="cn-beijing")
     {
         $this->accessKeyId = $accessKeyId;
         $this->accessKeySecret = $accessKeySecret;
