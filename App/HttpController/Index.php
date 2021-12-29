7a3b16ca10777a821c7b56a3680bba431e602492
lvzhenchao <1370164516@qq.com>
2021-12-29 22:58:59 +0800

    Update Index.php

--- App/HttpController/Index.php
+++ App/HttpController/Index.php
@@ -3,6 +3,7 @@
 namespace App\HttpController;
 
 use App\Lib\AliyunSdk\AliVod;
+use App\Lib\AliyunSdk2\AliVod2;
 use EasySwoole\Core\Http\AbstractInterface\Controller;
 class Index extends Controller
 {
@@ -12,7 +13,9 @@ class Index extends Controller
     }
 
     public function testali() {
-
+        $obj = new AliVod2();
+        $result = $obj->testUploadLocalVideo(\Yanconf::get("aliyun.accessKeyId"), \Yanconf::get("aliyun.accessKeySecret"),1);
+        print_r($result);
     }
 
 }
