f07da83ee6124e1d24d2c66dec3ba9147784d59b
lvzhenchao <1370164516@qq.com>
2021-12-30 16:30:09 +0800

    Update Index.php

--- App/HttpController/Index.php
+++ App/HttpController/Index.php
@@ -18,14 +18,14 @@ class Index extends Controller
         $videoName = "1.mp4";
         $result = $obj->createUploadVideo($title, $videoName);
 
-//        $uploadAddress = json_decode(base64_decode($result->UploadAddress), true);
-//
-//        $uploadAuth = json_decode(base64_decode($result->UploadAuth), true);
-//
-//        $obj->initOssClient($uploadAuth, $uploadAddress);
-//
-//        $videoFile = "/home/work/hdtocs/imooc/imooc_esapi/webroot/video/2018/10/7648e6280470bbbc.mp4";
-//        $result = $obj->uploadLocalFile($uploadAddress, $videoFile);
+        $uploadAddress = json_decode(base64_decode($result->UploadAddress), true);
+
+        $uploadAuth = json_decode(base64_decode($result->UploadAuth), true);
+
+        $obj->initOssClient($uploadAuth, $uploadAddress);
+
+        $videoFile = "/home/mycode/php_video/webroot/1.mp4";
+        $result = $obj->uploadLocalFile($uploadAddress, $videoFile);
         print_r($result);
     }
 
