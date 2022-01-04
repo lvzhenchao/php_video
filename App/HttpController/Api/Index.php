a07503d2340d89ec09203d34da52ad353ee17af7
lvzhenchao <1370164516@qq.com>
2022-01-04 23:12:19 +0800

    Update Index.php

--- App/HttpController/Api/Index.php
+++ App/HttpController/Api/Index.php
@@ -4,9 +4,66 @@ namespace App\HttpController\Api;
 
 use App\lib\Redis\Redis;
 use \EasySwoole\Core\Component\Di;
-
+use App\Model\Video as VideoModel;
 class Index extends Base
 {
+
+    /**
+     * 第一套方案 原始  - 读取 Mysql
+     * [lists description]
+     * @auth   singwa
+     * @return [type] [description]
+     */
+    public function lists0() {
+
+        $condition = [];
+        if(!empty($this->params['cat_id'])) {
+            $condition['cat_id'] = intval($this->params['cat_id']);
+        }
+        //
+        // 1 查询 条件 下 count
+        // 2 lists
+        //
+
+        try {
+            $videoModel = new VideoModel();
+            $data = $videoModel->getVideoData($condition, $this->params['page'], $this->params['size']);
+        }catch(\Exception $e) {
+            // $e->getMessage();
+            return $this->writeJson(Status::CODE_BAD_REQUEST, "服务异常");
+        }
+
+        if(!empty($data['lists'])) {
+            foreach($data['lists'] as &$list) {
+                //$data['lists'][$k]['create_time'] = date("Ymd H:i:s", $data['lists'][$k]['create_time']);
+                $list['create_time'] = date("Ymd H:i:s", $list['create_time']);
+                // 00:01:07
+                $list['video_duration'] = gmstrftime("%H:%M:%S", $list['video_duration']);
+            }
+        }
+        return $this->writeJson(Status::CODE_OK, "OK", $data);
+    }
+
+
+
+    /**
+     * 第二套方案 直接读取 静态化 json数据
+     * [lists description]
+     * @auth   singwa
+     * @return [type] [description]
+     */
+    public function lists() {
+        $catId = !empty($this->params['cat_id']) ? intval($this->params['cat_id']) : 0;
+        try {
+            $videoData = (new VideoCache())->getCache($catId);
+        }catch(\Exception $e) {
+            return $this->writeJson(Status::CODE_BAD_REQUEST , "请求失败");
+        }
+
+        $count = count($videoData);
+
+        return $this->writeJson(Status::CODE_OK, "OK", $this->getPagingDatas($count, $videoData));
+    }
 
     public function video() {
 //        new ab();
