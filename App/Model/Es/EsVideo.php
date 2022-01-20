2b652a3c175f8a3da9976266e6012dd6e6d926ff
lvzhenchao <1370164516@qq.com>
2022-01-20 22:16:25 +0800

    调整模型方法

--- App/Model/Es/EsVideo.php
+++ App/Model/Es/EsVideo.php
@@ -1,36 +1,10 @@
 <?php
 namespace App\Model\Es;
 
-use EasySwoole\Core\Component\Di;
-
-class EsVideo {
+class EsVideo extends EsBase {
 
     public $index = "imooc_video";
     public $type  = "video";
 
-    public function searchByName($name, $search_type = "match") {
-        $name = trim($name);
-        if (empty($name)) {
-            return [];
-        }
-
-        $params = [
-            "index" => $this->index,
-            "type"  => $this->type,
-            "body" => [
-                'query' => [
-                    $search_type => [
-                        'name' => $name
-                    ],
-                ],
-            ],
-        ];
-
-        $client = Di::getInstance()->get("ES");
-        $result = $client->search($params);
-
-
-        return $result;
-    }
 }
 
